f031173d7dd7c29e3407271e1992e83c1d926a07
janekbartnicki <jan.bartnicki01@gmail.com>
2023-09-24 16:36:25 +0200

    adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { motion } from "framer-motion"
-import Button from "./components/Button";
+import CodeWindow from "./components/CodeWindow";
 
 const App: React.FC = () => {
     return (
-        <>
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 100 }}
-                className="text-9xl"
-            >
-                Hi, i'm Janek
-            </motion.div>
-            <Button className="m-10" buttonContent={'Read more'}/>
-        </>
+        <CodeWindow/>
     );
 }
 
---
+++ src/components/CodeWindow.tsx
@@ -0,0 +1,9 @@
+import UpperNavBar from "./UpperNavBar";
+
+const CodeWindow: React.FC = () => {
+    return (
+        <UpperNavBar/>
+    )
+}
+
+export default CodeWindow;
